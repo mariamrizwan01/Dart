a32b2364583cc680c1b474686ce53499f58d7ddc
mariamrizwan01 <mariam.rizwan01@gmail.com>
2024-03-30 16:41:54 +0500

    List-Map-While Loop-for Loop-Variable-Operator

---
+++ 6) for loop.dart
@@ -0,0 +1,8 @@
+void main() {
+
+  //for loop 
+  var table = 6;
+  for (var i = 1; i <= 10; i++) {
+    print("$table x $i = ${i * table}");
+  }
+}
---
+++ 7) for-in loop.dart
@@ -0,0 +1,12 @@
+void main() {
+  //for in loop
+  List std = [
+    {"name":"areej","grade": "B"},
+    {"name":"eman","grade": "C"},
+    {"name":"mariam","grade": "A"},
+    {"name":"ali","grade": "B"},
+  ];
+  for (var abc in std) {
+    print(abc);
+  }
+}
