95fad8e6a2b3ada58c7d62e7b18a3eccefa9fc58
mariamrizwan01 <mariam.rizwan01@gmail.com>
2024-05-05 14:35:17 +0500

    Null Safety

--- 11) Null Safety.dart
+++ 11) Null Safety.dart
@@ -6,3 +6,11 @@ studentInfo({String? profilePicture}) {
   String info = profilePicture ?? "Default Profile";
   print(info);
 }
+
+// ---------------------- \\
+
+studentInfor({required num percentage}) {
+  String info = percentage >=50 ? "Pass" : "Fail";
+  print(info);
+}
+
